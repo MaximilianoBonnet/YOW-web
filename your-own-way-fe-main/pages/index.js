396efdd25a49c9d663bf7217e6a4e9105dcd2a4e
MaximilianoBonnet <maxbonn@hotmail.com>
2023-05-06 13:26:57 -0300

    commit

--- your-own-way-fe-main/pages/index.js
+++ your-own-way-fe-main/pages/index.js
@@ -1,4 +1,4 @@
-import { getFirestore } from "firebase/firestore";
+
 import { useEffect } from 'react';
 // import components
 import Hero from '../components/Hero';
@@ -12,28 +12,7 @@ import Footer from '../components/Footer';
 
 import Aos from 'aos';
 import 'aos/dist/aos.css';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAEicr-575Dzz1XFthW9fdTuRZaBoPS488",
-  authDomain: "yowinstitute.firebaseapp.com",
-  projectId: "yowinstitute",
-  storageBucket: "yowinstitute.appspot.com",
-  messagingSenderId: "887126197014",
-  appId: "1:887126197014:web:20f5963ad8b3a069498637",
-  measurementId: "G-LNR38014BB"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
 
 const Home = () => {
 
